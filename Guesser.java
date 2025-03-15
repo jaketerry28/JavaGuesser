fe6581ebe12d9e55f3a749ab777d3c6915f3fc5a
Jake Terry <47571311+jaketerry28@users.noreply.github.com>
2025-03-15 01:05:13 +0000

    final touches

--- Guesser.java
+++ Guesser.java
@@ -1,3 +1,5 @@
+//Guesser.java
+
 import java.util.*;
 import java.lang.*;
 
@@ -16,7 +18,7 @@ public class Guesser{
     public void menu(){
         boolean keepGoing = true;
         while (keepGoing){
-
+            
             System.out.println("0) Quit");
             System.out.println("1) Human Guesser");
             System.out.println("2) Computer Guesser");
@@ -28,10 +30,13 @@ public class Guesser{
                 keepGoing = false;
 
             } else if (response.equals("1")){
-                
+                System.out.println("Human Guesser!");
+                Human();
+
 
             } else if (response.equals("2")){
                 System.out.println("Computer Guesser!");
+                Computer();
 
             } else {
                 System.out.println("Invalid input");
@@ -39,10 +44,80 @@ public class Guesser{
         } // end while
     } // end menu
 
-    public boolean Human(){
+    public void Human(){
         int randInt = rand.nextInt(100) + 1; // randInt given random integer
-        System.out.println("Try to guess the number!");
-        System.out.println(randInt);   
-        String guess = input.nextLine();
+        System.out.println("Try to guess the number between 1 and 100!");
+        //System.out.println(randInt);   
+        boolean keepGoing = true;
+        int turns = 0;
+
+        while (keepGoing){
+            String guess = input.nextLine();
+
+            try {
+                int guessInt = Integer.parseInt(guess); // Convert input to int
+                if (guessInt < randInt) {
+                    System.out.println("Too low! Try again.");
+                    turns += 1;
+                } else if (guessInt > randInt){
+                    System.out.println("Too high! Try again.");
+                    turns += 1;
+                } else {
+                    turns += 1;
+                    System.out.println("Correct! It took you " + turns + " turns!");
+
+                    keepGoing = false;
+                } // end if
+            } catch (NumberFormatException e) {
+                if (guess.equalsIgnoreCase("q")){
+                    System.out.println("Quitting");
+                    keepGoing = false;
+                } else {               
+                 System.out.println("Invalid input! Please enter a number.");
+                } // end if
+            } // end catch
+        } // end while
     } // end Human
+
+    public void Computer(){
+        int lowerBound = 1;
+        int upperBound = 100;
+        int guess = 50;
+        int turns = 0;
+        System.out.println("Think of a number between 1 and 100 and the computer will guess it in 7 turns or less!");
+        System.out.println("Type H if too high, L if too low, or C if correct.");
+
+        boolean keepGoing = true;
+        while (keepGoing){
+            System.out.println("My guess is " + guess + ", is that too (H)igh, (L)ow, or (C)orrect?");
+            String answer = input.nextLine();
+
+            if (answer.equalsIgnoreCase("l")){
+                lowerBound = guess + 1; // update lower bound 
+                guess = (lowerBound + upperBound) / 2; // guess equals midpoint
+                turns += 1;
+            }
+
+            else if (answer.equalsIgnoreCase("h")){
+                upperBound = guess - 1; // update upper bound
+                guess = (lowerBound + upperBound) / 2; // guess equals midpoint
+                turns += 1;
+            }
+
+            else if (answer.equalsIgnoreCase("c")){
+                turns += 1;
+                System.out.println("I got it in " + turns + " turns!");
+                keepGoing = false;
+            }
+
+            else if (answer.equalsIgnoreCase("q")){
+                System.out.println("Goodbye!");
+                keepGoing = false;
+            } 
+
+            else {
+                System.out.println("Invalid input, try again please!");
+            } // end if 
+        } // end while
+    } // end Computer
 }
--- README.md
+++ README.md
@@ -1,9 +1,94 @@
 # JavaGuesser
+Create a guessing game.
 
-useful methods:
+Option 1 is to have the player guess a randomly generated number between 1-100.
+Option 2 is to have the computer guess a number the player picsk between 1-100.
+Will require a menu interface.
 
-public int compareTo(int anotherInt)
-    returns 0 if equal, less than 0 if lower, greater than if greater
+## Menu
 
-public int nextInt(int upperBound)
-    generates a value between 0 and upperBound -1
+```
+	while true
+		print quit
+		print Human Guesser
+		print Computer Guesser
+
+		take input from user
+
+		if the input equals 0
+			print goodbye
+			end while
+		
+		else if input is 1
+			print Human Guesser
+			Call Human()
+
+		else if input is 2
+			print Computer Guesser
+			Call Computer()
+
+		else 
+			print invalid input
+```
+
+## Human
+
+```
+public void Human()
+	generate a random integer between 1 and 100
+	print Try and guess the number between 1 and 100
+	
+	while true
+
+		take input from the user
+
+		try
+			convert input string into an int
+			if guess is less than random number
+				print too low
+			else if guess is greater than random number
+				print too high
+			else 
+				print correct
+				end while
+		catch exception
+			if guess equals q 
+				end while loop
+			else 
+				print invalid input
+```
+## Computer
+
+```
+public void Computer 
+	initialize integer lowerbound is 1
+	initialize integer upperbound is 100
+	initialize integer guess is 50
+	
+	print out rules of the game
+	print out how to tell the computer too high, low, or correct
+	
+	while true
+		print computers guess
+		take input from the user
+		
+		if input equals L
+			lowerBound updates to value of guess + 1
+			guess updates to (lowerBound + upperBound) / 2
+		
+		else if input equals H
+			upperBound updates to value of guess - 1
+			guess updates to (lowerBound + upperBound) / 2
+		
+		else if input equals C
+			print out correct
+			end while loop
+
+		else if input equals q
+			print out quitting
+			end while loop
+
+		else 
+			print out invalid input
+
+
